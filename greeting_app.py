2ec226b6a9f9561b049e8132e4d9361f9d219fda
sujandai <sujankc3144@gmail.com>
2025-05-20 19:16:13 -0400

    Second feature addition to the greeting application.

--- greeting_app.py
+++ greeting_app.py
@@ -1,2 +1,4 @@
 print("Welcome to the Greeting Application.")
 print("This application will greet the customers.")
+print("The owner of this application is Transcend Team.")
+print("This app will be the main revenue source for the company.")
